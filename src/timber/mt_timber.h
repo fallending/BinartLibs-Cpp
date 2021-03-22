7e5d6060136ab35820cd80954b23afca0ddcc55f
fallen ink <fengzilijie@qq.com>
2021-03-22 19:21:15 +0800

    ready to add timber for logging plugin

---
+++ src/timber/mt_timber.h
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-22 17:16:11
+ * @LastEditTime: 2021-03-22 18:25:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /mt-ccs/src/timber/mt_timber.h
+ */
+
+#ifndef __MT_TIMBER_H__
+#define __MT_TIMBER_H__
+
+
+namespace timber
+{
+    class Timber
+    {
+    private:
+        /* data */
+    public:
+        Timber(/* args */) {};
+        ~Timber() {};
+    };
+    
+} // namespace timber
+
+
+#endif // __MT_TIMBER_H__
---
+++ src/timber/mt_tree.h
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-22 18:22:11
+ * @LastEditTime: 2021-03-22 18:24:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /mt-ccs/src/timber/mt_tree.h
+ */
+
+#ifndef __MT_TREE_H__
+#define __MT_TREE_H__
+
+namespace timber
+{
+    class Tree
+    {
+    private:
+        /* data */
+    public:
+        Tree(/* args */) {};
+        ~Tree() {};
+    };    
+} // namespace timber
+
+
+#endif // __MT_TREE_H__
